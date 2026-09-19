bc74399f629a6994e1eb7b28616533a380839857
Angel Gabriel Valenzuela Barrera <avalenzuelab3@miumg.edu.gt>
2026-09-19 10:39:20 -0600

    feat(ui): complete las mejoras del segundo parcial

--- empleados-ui/src/main/java/edu/umg/programacion2/empleados/ui/EmpleadoTableModel.java
+++ empleados-ui/src/main/java/edu/umg/programacion2/empleados/ui/EmpleadoTableModel.java
@@ -76,6 +76,9 @@ public class EmpleadoTableModel extends AbstractTableModel {
 
             default:
                 return "";
+                
+                
+                
         }
     }
 }
